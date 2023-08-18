525cf4eac09c0f75813aa97bc2259233dc0961ac
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-08-18 17:56:36 +0900

    유저닉네임변경시유저리스트 등록

--- src/main/java/com/example/exception/websocket/controller/ChatController.java
+++ src/main/java/com/example/exception/websocket/controller/ChatController.java
@@ -2,15 +2,21 @@ package com.example.exception.websocket.controller;
 
 
 import com.example.exception.websocket.model.Message;
+import com.example.exception.websocket.model.Userlist;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @Slf4j
 public class ChatController {
+
+    private final List<String> userList = new ArrayList<>();
     @MessageMapping("/chat")
     @SendTo("/topic/public")
     public Message broadcast(@Payload Message message) {
@@ -18,4 +24,13 @@ public class ChatController {
         log.info("message = {}",message);
         return message;
     }
+    @MessageMapping("/chat/userlist")
+    @SendTo("/topic/public/userlist")
+    public List<String> addUser(@Payload Userlist userlist) {
+        userList.add(userlist.getCurrentUser());
+        userList.remove(userlist.getPrevUser());
+        log.info("userList = {}",userList);
+        return userList;
+    }
+
 }
